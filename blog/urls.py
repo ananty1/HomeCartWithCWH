1d30312878ea5a4a7b17edecae823699aa39c41a
anant <ananty@iitbhilai.ac.in>
2024-02-25 22:32:26 +0530

    Media not working on vercel,making temporary solutin

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.index,name='BlogHome'),
     path('blogpost/<int:id>',views.blogpost,name='BlogPost'),
 ]
+urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
